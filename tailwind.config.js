68fef3247cde5d7f466fe4061fcaf27f5b2f9a71
LucasPetruci <lucaspetruci100@gmail.com>
2024-08-01 17:10:25 -0300

    chore: Add brand color and update dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "brand-900": "#42307D",
+      },
+    },
   },
   plugins: [],
 };
